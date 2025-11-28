f092dd80c4ff7c098d5bd639289d48749f90c6c4
Vivek P Prakash <vivekpprakash@xeoscript.com>
2025-11-28 18:55:33 +0530

    Changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
-from .models import User
 from django import forms
 
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserForm(forms.ModelForm):
