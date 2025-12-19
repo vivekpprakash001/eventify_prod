d1e618e06b60db66f7571e232436236c1848bf10
Vivek <kandoewinpvtltd@Viveks-Macbook-Pro.local>
2025-12-20 02:28:15 +0530

    Fixes for the icon in the eventtypes

--- master_data/forms.py
+++ master_data/forms.py
@@ -5,7 +5,12 @@ from .models import EventType
 class EventTypeForm(forms.ModelForm):
     class Meta:
         model = EventType
-        fields = ['event_type']
+        fields = ['event_type', 'event_type_icon']
         widgets = {
-            'event_type': forms.TextInput(attrs={'class': 'form-control'}),
+            'event_type': forms.TextInput(attrs={'class': 'form-control'}), 
+            'event_type_icon': forms.FileInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'event_type': 'Event Type',
+            'event_type_icon': 'Event Type Icon',
         }
